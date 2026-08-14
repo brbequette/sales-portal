fba7f57cc26703150652a8d62ffc3c1d2bb20de5
Benjamin Bequette <futureoffactory@gmail.com>
2026-08-14 12:12:25 -0700

    chore: remove temp query files

--- _tmp_query.js
+++
@@ -1,8 +0,0 @@
-const{PrismaClient}=require('@prisma/client'); 
-const p=new PrismaClient(); 
-async function main(){ 
-  const pkgs=await p.(SELECT id," "packageNumber,status,trackingNumber,carrier,updatedAt FROM Package WHERE status='shipped' AND updatedAt>'2026-08-14' ORDER BY updatedAt DESC); 
-  console.log(JSON.stringify(pkgs,null,2)); 
-  await p.(); 
-} 
-main().catch(e=>{console.error(e);process.exit(1)});
