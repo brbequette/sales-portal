6ec8846ea6c8b2e37b53847915aecd4258f8946b
Benjamin Bequette <futureoffactory@gmail.com>
2026-08-13 21:09:53 -0700

    chore: Remove scratch scripts

--- fix.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs'); 
-let c = fs.readFileSync('src/components/DashboardView.tsx', 'utf8'); 
-c = c.replace(/\} role="button"/g, ')} role="button"'); 
-c = c.replace(/\}\}\} \)/g, '}}}'); 
-c = c.replace(/\}\}\}\)/g, '}}}'); 
-fs.writeFileSync('src/components/DashboardView.tsx', c);
